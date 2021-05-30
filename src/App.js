37590f7e991474eb04edc33356ec50a6f3bfe7ff
Sandeep Sharma <sandy335582@gmail.com>
2021-05-30 10:46:08 +0530

    travis-test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p> React app</p>
+        <p>Travis CI test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
